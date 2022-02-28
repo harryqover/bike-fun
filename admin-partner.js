286cb65d64f700fa0e6513552a4e6ab619fb23af
harryqover <harry@qover.com>
2022-02-28 06:52:22 +0100

    Update admin-partner.js

--- admin-partner.js
+++ admin-partner.js
@@ -215,7 +215,7 @@ function showGeneralInfo(){
   $(".hide-when-loading").hide();
   $(".form-check-validity-voucher").hide();
   $(".title-platform, .breadcrumb-here").text("General information");
-  var allHTML = '<h3>Welcome to our general info page</h3><p>this is your promocode</p>';
+  var allHTML = '<div class="generalInfoContent"><h3>Welcome to our general info page</h3><p>this is your promocode</p></div></div>';
   $(".block-in-content-platform").html(allHTML);
   $(".hide-when-loading").show();
   $(".loading").hide();
