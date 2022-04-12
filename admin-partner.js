9091f4c0ddfa049523d0a7b32f9e52394fba644e
harryqover <harry@qover.com>
2022-04-12 07:28:35 +0200

    Update admin-partner.js

--- admin-partner.js
+++ admin-partner.js
@@ -13,7 +13,7 @@
   } else {
     $(".login-container").show();
     $(".form-check-validity-voucher,#nav").hide();
-    window.location.href = 'https://bike-a5adfd.webflow.io/login-partner';
+    window.location.href = 'https://bike.qoverme.com/login-partner';
   }
 
 
