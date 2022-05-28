11ac8c7151260719891428a1f1930908a28d007e
harryqover <harry@qover.com>
2022-05-28 11:32:08 +0200

    Update admin-partner.js

--- admin-partner.js
+++ admin-partner.js
@@ -310,6 +310,10 @@ function getstatistics(start, end) {
         //var datajson = JSON.parse(data);
         let todayX = new Date();
         todayX.toISOString().split('T')[0];
+        let minDate = new Date()
+        minDate.setMonth(minDate.getMonth() - 3);
+        minDate.toISOString().split('T')[0]
+
         console.warn(data);
         console.log(data.kpi.kpis);
         console.log(data.kpi.kpis.EUR);
@@ -332,7 +336,7 @@ function getstatistics(start, end) {
               var ttlcommission = data.kpi.kpis.EUR.tot_commission;
               var allHTML = '<div class="allKpis"><div class="reportingKpi"><h5>Nouveaux contrats</h5><p>' + data.kpi.kpis.EUR.nbr_create + '</p></div><div class="reportingKpi"><h5>Contrats annulés</h5><p>' + data.kpi.kpis.EUR.nbr_cancel + '</p></div><div class="reportingKpi"><h5>Commission totale</h5><p>€' + ttlcommission.toFixed(2) + '</p></div></div><div id="chart_div"></div>';
               $(".block-in-content-platform").html(allHTML);
-              var dateInput = '<div class="formDateInput"><input type="date" id="start" value="2022-01-01" min="2020-01-01" max="2030-12-31"><input type="date" id="end" value="'+todayX+'" min="2020-01-01" max="2030-12-31"><br><br><button onclick="getStatFromInput();" class="btnPink">Recherche</button></div>';
+              var dateInput = '<div class="formDateInput"><input type="date" id="start" value="'+minDate+'" min="2020-01-01" max="'+todayX+'"><input type="date" id="end" value="'+todayX+'" min="2020-01-01" max="'+todayX+'"><br><br><button onclick="getStatFromInput();" class="btnPink">Recherche</button></div>';
               $(".block-in-content-platform").prepend(dateInput);
               window.bdxRows = JSON.parse(data.bdx.data.EUR);
               bdxRows = [
@@ -386,7 +390,7 @@ function getstatistics(start, end) {
           $('head').append(css);
           var allHTML = '<div class="allKpis"><div class="reportingKpi"><h5>Nouveaux contrats</h5><p>0</p></div><div class="reportingKpi"><h5>Contrats annulés</h5><p>0</p></div><div class="reportingKpi"><h5>Commission totale</h5><p>€ 0.00</p></div></div><div id="chart_div"></div>';
           $(".block-in-content-platform").html(allHTML);
-          var dateInput = '<div class="formDateInput"><input type="date" id="start" value="2022-01-01" min="2020-01-01" max="2030-12-31"><input type="date" id="end" value="'+todayX+'" min="2020-01-01" max="2030-12-31"><br><br><button onclick="getStatFromInput();" class="btnPink">Recherche</button></div>';
+          var dateInput = '<div class="formDateInput"><input type="date" id="start" value="'+minDate+'" min="2020-01-01" max="'+todayX+'"><input type="date" id="end" value="'+todayX+'" min="2020-01-01" max="'+todayX+'"><br><br><button onclick="getStatFromInput();" class="btnPink">Recherche</button></div>';
           $(".block-in-content-platform").prepend(dateInput);
           $(".loading").hide();
           $(".hide-when-loading").show();
