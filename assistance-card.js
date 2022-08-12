e8bbd60d01aa319e2f4f7d1b08e016fc7e125f8d
harryqover <harry@qover.com>
2022-08-12 10:28:55 +0200

    Update assistance-card.js

--- assistance-card.js
+++ assistance-card.js
@@ -1,1 +1,24 @@
 console.log("hello");
+
+let country = getParameterByName("country");
+
+
+let assistancePhone = {
+	"BE" : "+32 2 541 92 01",
+	"FR" : "+33 9 78 46 61 24",
+	"DE" : "+49 800 589 39 21",
+	"NL" : "020 532 07 06"
+}
+
+let qoverPhone = {
+	"BE" : "+32 2 541 92 01",
+	"FR" : "+33 9 78 46 61 24",
+	"DE" : "+49 800 589 39 21",
+	"NL" : "020 532 07 06"
+}
+
+
+$("[data-phone='qover-text']").text(qoverPhone[country]);
+$("[data-phone='assistance-text']").text(assistancePhone[country]);
+$("[data-phone='qover-link']").attr("href", "tel:"+qoverPhone[country]);
+$("[data-phone='assistance-link']").attr("href", "tel:"+assistancePhone[country]);
