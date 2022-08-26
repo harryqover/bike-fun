3bb244ebd2bc162aa2c8ccf7e812d49c463907c1
harryqover <harry@qover.com>
2022-08-26 08:08:27 +0200

    Update admin-partner.js

--- admin-partner.js
+++ admin-partner.js
@@ -469,7 +469,7 @@ function findInvoices() {
                 //var codeUsed5Characters = "xxxx-xxxx-xxxx" + codeUsed.substr(codeUsed.length - 5);
                 //var dateNotFormatted = new Date(data[i].dateUsed);
                 //var dateFormatted = dateNotFormatted.toLocaleDateString("en-BE");
-                var htmllinevoucher = '<tr class="voucher-line"><td>' + data[i].invONumber + '</td><td>' + data[i].invONumber + '</td><td>' + data[i].invONumber + ' ' + data[i].invONumber + '</td><td>' + data[i].invONumber + '</td><td>' + data[i].invONumber + '</td><td>' + data[i].invONumber + '</td></tr>';
+                var htmllinevoucher = '<tr class="voucher-line"><td>' + data[i].invONumber + '</td><td>' + data[i].invOAmount + '</td><td>' + data[i].invOStatus + ' ' + data[i].invONumber + '</td><td>' + data[i].invONumber + '</td><td>' + data[i].invONumber + '</td><td>' + data[i].invONumber + '</td></tr>';
                 allHTML = allHTML + htmllinevoucher;
             }
             allHTML = allHTML + "</table>";
