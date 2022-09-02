25f3d6f046ff561e1c37a5cad9a966cb756683e3
harryqover <harry@qover.com>
2022-09-02 08:54:27 +0200

    Update qover-popup.js

--- qover-popup.js
+++ qover-popup.js
@@ -42,12 +42,15 @@ var typeInfo = {
 var text = '<a onclick="showModalPopUpQover()" style="text-decoration: underline;">'+typeInfo[type].txtCta[lang]+'<svg class="info-icon" width="13" height="13" viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg" fill="#323232" tabindex="0"><g fill="inherit" fill-rule="even-odd"><path d="M6.5.91a5.59 5.59 0 110 11.182A5.59 5.59 0 016.5.91zm0-.91a6.5 6.5 0 100 13 6.5 6.5 0 000-13z"></path><path d="M6.525 3.248c.258.016.514.047.767.092.776.18 1.254.748 1.338 1.52a1.396 1.396 0 01-.561 1.323c-.2.158-.41.302-.61.457a.953.953 0 00-.43.88.551.551 0 01-.395.561.657.657 0 01-.755-.186.55.55 0 01-.095-.28 1.871 1.871 0 01.767-1.626c.225-.175.437-.366.633-.57a.666.666 0 00.156-.391c.02-.389-.252-.629-.675-.64-.512-.014-.937.127-1.222.566a.617.617 0 01-.618.296c-.435-.053-.632-.381-.468-.772.172-.382.48-.694.87-.88a3.277 3.277 0 011.298-.35z"></path><circle cx="6.5" cy="9.4" r="1"></circle></g></svg></a>';
 
 
-var modal = '<div id="modal-form" class="modal-issue"><div class="modal-content"><div class="modal-header"><span class="close">&times;</span></div><div class="modal-body"><iframe class="airtable-embed" src="'+typeInfo[type].url+'?lang='+lang+'" frameborder="0" onmousewheel="" height="650px" style="background: transparent; border-radius: 5px;" width="100%"></iframe></div></div><</div>'
+var modal = '<div id="modal-form" class="modal-issue"><div class="modal-content"><div class="modal-header"><span onclick="closeModal()" class="close">&times;</span></div><div class="modal-body"><iframe class="airtable-embed" src="'+typeInfo[type].url+'?lang='+lang+'" frameborder="0" onmousewheel="" height="650px" style="background: transparent; border-radius: 5px;" width="100%"></iframe></div></div><</div>'
 var modalCss = '<style>.modal-issue{display:none;position:fixed;z-index:999;left:0;top:0;width:100%;height:100%;overflow:auto;background-color:rgba(0,0,0,.5)}.close{color:#000;float:right;font-size:50px;font-weight:700}.close:hover,.close:focus{color:#a8818b;text-decoration:none;cursor:pointer;-webkit-transition:all 200ms ease-in;transition:all 200ms ease-in}.modal-header{padding:2px 16px;color:#fff}.modal-body{padding:2px 16px;max-width:794px}.modal-content{position:relative;background-color:transparent;margin:auto;padding:0;width:50%;-webkit-animation-name:animatetop;-webkit-animation-duration:0.4s;animation-name:animatetop;animation-duration:0.4s}@-webkit-keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}@keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}</style>';
 modal = modal+modalCss;
 
 
-
+function closeModal(){
+	var modal = document.getElementById('modal-form');
+	modal.style.display = "none";
+}
 
 function modalTest (){
 	// Get the modal
