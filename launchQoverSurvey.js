acdb228bed0b90952a50b9cf2d6db2440a80d975
harryqover <harry@qover.com>
2022-10-24 14:14:58 +0200

    Update launchQoverSurvey.js

--- launchQoverSurvey.js
+++ launchQoverSurvey.js
@@ -1,20 +1,4 @@
 
 
 
-
-    
-    <script src="https://form.jotform.com/static/feedback2.js" type="text/javascript">
-      new JotformFeedback({
-        formId: "222863802419054",
-        buttonText: "Survey Owlee",
-        base: "https://form.jotform.com/",
-        background: "#4a90e2",
-        fontColor: "#FFFFFF",
-        buttonSide: "left",
-        buttonAlign: "center",
-        type: false,
-        width: 700,
-        height: 500,
-        isCardForm: false
-      });
-    </script>
+console.log("qoverSurveyStart");
