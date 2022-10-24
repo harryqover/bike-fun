11126c2d9576712c8f4d513380214169c99a8b3f
harryqover <harry@qover.com>
2022-10-24 14:24:24 +0200

    Update launchQoverSurvey.js

--- launchQoverSurvey.js
+++ launchQoverSurvey.js
@@ -2,3 +2,12 @@
 
 
 console.log("qoverSurveyStart");
+
+
+if(window.location.pathname == "/fr-fr" || window.location.pathname == "/fr-be"){
+  var banner = '<div class="banner-qoverme-becomes-qover wf-section"><div class="container-111 w-container"><div class="text-block-214"><a href="" target="_blank">Participez à notre questionnaire vol de vélo</a></div></div></div>';
+  $( ".body-9" ).prepend( banner );
+}
+
+
+
