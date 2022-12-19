ca90ac4143ba1c44cb7dfa2512222945b14deb77
harryqover <harry@qover.com>
2022-12-19 11:24:50 +0100

    Update customer-portal.js

--- customer-portal.js
+++ customer-portal.js
@@ -1,6 +1,15 @@
 var cowboyIds = ["60a75f9f987d3f484ed24ef4", "607937e4654780240a132641", "60938efba79100e71519a03b", "5ff6cf4fceba6039aadb446f", "61b1c260415df342d60f4e10", "61b1b145415df342d60f4e0f", "61b1d0a02656f6227dc3476f", "61b8a43042cef3c0bc2cc26d", "61b8a49f11e584fcae0ee070", "61b8a45842cef3c0bc2cc26e", "61b8a4c211e584fcae0ee071", "61b8a4e807007c0a5b94d673", "61b8a51111e584fcae0ee072", "61b8a52111e584fcae0ee073"];
 var cowboyAlteosIds = ["5ff6cf4fceba6039aadb446f", "60938efba79100e71519a03b", "607937e4654780240a132641", "61b1b145415df342d60f4e0f", "61b1c260415df342d60f4e10", "60a75f9f987d3f484ed24ef4"]
 
+$("#connected").hide();
+$("#disconnected").show();
+
+var login = getCookie("login");
+var cigarId = getCookie("cigarId");
+
+if(login && cigarId){
+  login(cigarId, email);
+}
 
 function login(cigarId, email) {
     var data = JSON.stringify({
@@ -16,6 +25,15 @@ function login(cigarId, email) {
 
     xhr.addEventListener("readystatechange", function() {
         if (this.readyState === 4 && this.status === 200) {
+
+            var timeToAdd = 1000 * 60 * 60 * 24 * 1 * 1 * 1;
+            var date = new Date();
+            var expiryTime = parseInt(date.getTime()) + timeToAdd;
+            date.setTime(expiryTime);
+            var utcTime = date.toUTCString();
+            document.cookie = "login="+email+"; expires=" + utcTime + ";";
+            document.cookie = "cigarId="+cigarId+"; expires=" + utcTime + ";";
+
             var obj = JSON.parse(this.responseText);
             var partnerId = obj.partnerId;
             var variant = obj.variant;
@@ -39,9 +57,13 @@ function login(cigarId, email) {
             }
 
             /* YOU ARE LOGGED IN*/
+            $("#connected").show();
+            $("#disconnected").hide();
 
 
         } else if (this.readyState === 4 && this.status === 400) {
+            $("#connected").hide();
+            $("#disconnected").show();
             console.log(this.status);
             var obj = JSON.parse(this.responseText);
             alert(obj.message);
