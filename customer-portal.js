971c7d50d9969be11976b6ec9ee0207e9ab00913
harryqover <harry@qover.com>
2022-12-19 13:29:13 +0100

    Update customer-portal.js

--- customer-portal.js
+++ customer-portal.js
@@ -88,6 +88,8 @@ function goLogin(cigarId, email) {
             $("[data-var='start']").text(start.toLocaleDateString());
             $("[data-var='end']").text(end.toLocaleDateString());
 
+            $("[data-var='cancel']").attr("href","https://form.jotform.com/222763047790359?lang=en&contractid="+cigarId"&email="+email);
+
             var responseTest = {"country":"BE","depreciation":false,"damageDeductible":"DAMAGE_DEDUCTIBLE_ENGLISH_10PC","endDate":"2023-08-07T21:59:59.999Z","originalValue":115200,"partnerId":"5e7a295467920985a134f426","startDate":"2022-08-07T22:00:00.000Z","status":"STATUS_OPEN","theftDeductible":"THEFT_DEDUCTIBLE_STANDARD_10PC","variant":"VARIANT_THEFT_DAMAGE_ASSISTANCE"};
 
             /* YOU ARE LOGGED IN*/
