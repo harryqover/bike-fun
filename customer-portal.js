81ddd10fab82de58d5c7384322badc6bbd798bee
harryqover <harry@qover.com>
2022-12-21 21:38:18 +0100

    Update customer-portal.js

--- customer-portal.js
+++ customer-portal.js
@@ -228,3 +228,23 @@ function logout(){
     $(".loading").hide();
     $("#disconnected").show();
 }
+
+function translateAll(){
+  let xhrLocales = new XMLHttpRequest();
+  var content = "";
+  xhrLocales.open("get", "https://translations.qover.com/widget/" + lang + "-raw.json?refresh=007", true);
+  xhrLocales.setRequestHeader("Cache-Control", "max-age=3600");
+
+  xhrLocales.onreadystatechange = function() {
+    if (xhrLocales.readyState == 4) {
+      if (xhrLocales.status >= 200 && xhrLocales.status < 300 || xhrLocales.status == 304) {
+        content = JSON.parse(xhrLocales.responseText);
+        $( "[data-translation]" ).each(function( index ) {
+          $( this ).html(content[$( this ).data( "translation" )]);
+          var text = $( this ).html();
+        });
+      }
+    }
+  };
+  xhrLocales.send();
+}
