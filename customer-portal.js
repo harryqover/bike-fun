68bbe5827f51b72b0102e4d1628b94df178801d1
harryqover <harry@qover.com>
2022-12-21 16:33:20 +0100

    Update customer-portal.js

--- customer-portal.js
+++ customer-portal.js
@@ -1,4 +1,4 @@
-console.warn("v20221221 1627");
+console.warn("v20221221 1635");
 const cowboyIds = ["60a75f9f987d3f484ed24ef4", "607937e4654780240a132641", "60938efba79100e71519a03b", "5ff6cf4fceba6039aadb446f", "61b1c260415df342d60f4e10", "61b1b145415df342d60f4e0f", "61b1d0a02656f6227dc3476f", "61b8a43042cef3c0bc2cc26d", "61b8a49f11e584fcae0ee070", "61b8a45842cef3c0bc2cc26e", "61b8a4c211e584fcae0ee071", "61b8a4e807007c0a5b94d673", "61b8a51111e584fcae0ee072", "61b8a52111e584fcae0ee073"];
 const cowboyAlteosIds = ["5ff6cf4fceba6039aadb446f", "60938efba79100e71519a03b", "607937e4654780240a132641", "61b1b145415df342d60f4e0f", "61b1c260415df342d60f4e10", "60a75f9f987d3f484ed24ef4"]
 
@@ -185,13 +185,17 @@ function getNinjaData(cigarId, email){
     $("[data-var='serial']").text(response.payload.risk.serialNumber);
     $("[data-var='price']").text("EUR "+response.payload.price/100);
     $("[data-var='status']").text(statusContract[response.payload.status]);
+    
 
     if(response.payload.status == "STATUS_OPEN" && !response.payload.versionInfo.cancelInformation){
         console.log("full active")
+        $("[data-var='renewal']").text("renewed");
     } else if(response.payload.status == "STATUS_OPEN" && response.payload.versionInfo.cancelInformation.requestCancelAtRenewal == true) {
         console.log("active but cancel at renewal")
+        $("[data-var='renewal']").text("cancelled");
     } else if(response.payload.status == "STATUS_CLOSED") {
         console.log("closed");
+        $("[data-var='renewalorcanceltext']").text("Cancelled on "+response.payload.versionInfo.cancelInformation.effectiveDate);
     } else {
         console.log("something else: "+response.payload.status+" - "+response.payload.versionInfo);
     }
