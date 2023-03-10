de9d99e8d1f3184e09ecf67d2a40f84e1efe8df8
harryqover <harry@qover.com>
2023-03-10 15:06:06 +0100

    Update launchQoverSurvey.js

--- launchQoverSurvey.js
+++ launchQoverSurvey.js
@@ -1,9 +1,10 @@
 console.log("qoverSurveyStart 20221024");
 
+/*<script src ="https://forms.qover.com/static/feedback2.js" type="text/javascript"></script> */
 
 if(window.location.pathname == "/fr-fr" || window.location.pathname == "/fr-be"){
-  var banner = '<div class="banner-qoverme-becomes-qover wf-section"><div class="container-111 w-container"><div class="text-block-214"><a href="https://eu.jotform.com/222863802419054" target="_blank">Participez à notre questionnaire vol de vélo</a></div></div></div>';
-  $( ".body-9" ).prepend( banner );
+  var banner = '<div class="banner-qoverme-becomes-qover wf-section"><div class="container-111 w-container"><div class="text-block-214"><a href="https://forms.qover.com/230683603807356" target="_blank">Participez à notre enquête</a></div></div></div>';
+  $( ".body-9" ).prepend( banner );    
 }
 
 
