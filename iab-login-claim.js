eab3495fa48c8deb1ae9e9370de3a8724b101c99
harryqover <harry@qover.com>
2023-05-15 14:41:57 +0200

    login iab portal

--- iab-login-claim.js
+++ iab-login-claim.js
@@ -3,6 +3,27 @@ console.warn("v20230515 1246");
 const isValidCigar = (str) => /^[A-Za-z]{2}20\d{9}$/.test(str);
 const isValidEmail = (str) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(str);
 
+const baseUrlClaim = {
+	"MAKE_TESLA": "https://insuremytesla.qover.com/claims"
+}
+
+$("#disconnected").hide();
+
+function loadAtStart(){
+	var contract = getParameterByName("policy_reference");
+	if(contract == ""){
+		//show login
+	} else {
+		//show claim form
+
+		/*START IMPORT JOTFORM SCRIPT*/
+		var jf = document.createElement('script');
+		jf.src = "https://form.jotform.com/jsform/211861951435356";
+		document.getElementsByTagName('head')[0].appendChild(jf);
+		/*END IMPORT  JOTFORM SCRIPT*/
+	}
+}
+
 function clickToLogin() {
     var cigarId = $('input[name="name"]').val();
     var email = $('input[name="email"]').val();
@@ -44,7 +65,19 @@ function getNinjaData(cigarId, email) {
         if(response.payload == "error"){
             console.warn("error while trying to connect");
         } else {
-        	window.location.href = "https://insuremytesla.qover.com/claims?language=fr";
-        }
-    }
+        	var lang = response.payload.language;
+        	var vin = response.payload.risk.vin;
+        	var brand = response.payload.risk.make;
+        	window.location.href = baseUrlClaim[brand]+"?language="+lang+"&claimant_email="+email+"&policy_reference="+cigarId+"&vehicle_plate_number="+vin;
+        }	
+    })
 }
+
+/*
+TO INCLUDE IN FORM REDIRECT
+
+claimant_email
+policy_reference
+vehicle_plate_number
+ref_country
+*/
