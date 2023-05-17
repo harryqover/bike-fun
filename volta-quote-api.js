de03675254481e68875fb977e26ae2c7f42c7524
harryqover <harry@qover.com>
2023-05-17 14:37:50 +0200

    Update volta-quote-api.js

--- volta-quote-api.js
+++ volta-quote-api.js
@@ -1,3 +1,17 @@
+
+
+function clickToGetPrice(){
+	var truckAmount = 1;
+	var truckType = "18tRefriBi";
+	var dangerousGoodsTrucks = true;
+	var leasedTrucks = true;
+	var deductible_MTPL = "2000EUR";
+	var deductible_PartialCasco = "2000EUR";
+	var deductible_CollisionCasco = "2000EUR";
+	getPrice(truckAmount, truckType, dangerousGoodsTrucks, leasedTrucks, deductible_MTPL, deductible_PartialCasco, deductible_CollisionCasco);
+}
+
+
 function getPrice(truckAmount, truckType, dangerousGoodsTrucks, leasedTrucks, deductible_MTPL, deductible_PartialCasco, deductible_CollisionCasco) {
     var googleSheetUrl = "https://script.google.com/macros/s/AKfycbxd7iLSKEWjn4Kjsh6SR4zVDZCz2HxnAXA1OHG_7pzBrE0VLi5ze-9DOV1Y7tpyr13d6Q/exec";
 
@@ -22,7 +36,9 @@ function getPrice(truckAmount, truckType, dangerousGoodsTrucks, leasedTrucks, de
     $.ajax(settings).done(function(response) {
             console.log(response);
             window.payloadFromNinja = response;
+            var formattedTotalPrice = response.response.total.toLocaleString("en-BE", {style: "currency", currency: "EUR"});
+            
+            $("#form-quote > div.flex-v-25.margin-bottom-60 > div.price > div:nth-child(2)").text(formattedTotalPrice);
         }
     )
 }
-getPrice(1, "18tRefriBi", true, true, "2000EUR", "2000EUR", "2000EUR");
