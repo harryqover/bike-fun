83a1b958224f51690da7d969cfe901f33bbffa7c
harryqover <harry@qover.com>
2023-06-20 13:03:36 +0200

    Update apiLiveDamage.js

--- apiLiveDamage.js
+++ apiLiveDamage.js
@@ -1,37 +1,3 @@
-/* OLD CODE
-const apiDamage = "https://script.google.com/macros/s/AKfycbzzVrd1MmC3pHOWZJ7RIEBbTwtxs-8XYN23E16oQ7yix0bLlR1mxSAt3JkEoNvnkwzq/exec";
-
-var settings = {
-	"url": apiDamage,
-	"method": "POST",
-	"timeout": 0,
-	"headers": {
-	    "Content-Type": "text/plain;charset=utf-8"
-	},
-	"data": JSON.stringify({
-	    "action": "getLiveDamage"
-	}),
-};
-
-$.ajax(settings).done(function(response) {
-	console.log(response.total);
-	var total = response.total;
-	total = total.toLocaleString();
-	console.log(total);
-	$("[data-var='live-damage']").text(total);
-})
-
-
-
-function animateLoading() {
-  dots++;
-  if (dots > 3) dots = 0;
-
-  const dotsString = '.'.repeat(dots);
-  $("[data-var='live-damage']").innerHTML = `${dotsString}`;
-}
-*/
-
 const apiDamage = "https://script.google.com/macros/s/AKfycbzzVrd1MmC3pHOWZJ7RIEBbTwtxs-8XYN23E16oQ7yix0bLlR1mxSAt3JkEoNvnkwzq/exec";
 
 const tradSocialProof = {
