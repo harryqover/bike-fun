c34b9307045c9a07d7ae1d3e6703cd5bb5e58006
harryqover <harry@qover.com>
2023-07-10 16:05:47 +0200

    Update customer-portal-bike.js

--- customer-portal-bike.js
+++ customer-portal-bike.js
@@ -856,8 +856,9 @@ function getAllInputsChecked (){
 
         var loadingPricesDiv = '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>';
         loadingPricesDiv = loadingPricesDiv + '<style>.lds-ellipsis {display: inline-block;position: relative;width: 80px;height: 80px;}.lds-ellipsis div {position: absolute;top: 33px;width: 13px;height: 13px;border-radius: 50%;background: #000;animation-timing-function: cubic-bezier(0, 1, 1, 0);}.lds-ellipsis div:nth-child(1) {left: 8px;animation: lds-ellipsis1 0.6s infinite;}.lds-ellipsis div:nth-child(2) {left: 8px;animation: lds-ellipsis2 0.6s infinite;}.lds-ellipsis div:nth-child(3) {left: 32px;animation: lds-ellipsis2 0.6s infinite;}.lds-ellipsis div:nth-child(4) {left: 56px;animation: lds-ellipsis3 0.6s infinite;}@keyframes lds-ellipsis1 {0% {transform: scale(0);}100% {transform: scale(1);}}@keyframes lds-ellipsis3 {0% {transform: scale(1);}100% {transform: scale(0);}}@keyframes lds-ellipsis2 {0% {transform: translate(0, 0);}100% {transform: translate(24px, 0);}}</style>'
-        $(".cancelFormQuestions").attr("style", "transition: opacity 0.5s; opacity:0%;")
+        $(".cancelFormQuestions").attr("style", "transition: opacity 0.5s; opacity:0%;");
         $("#blockStartForm").html(loadingPricesDiv);
+        $(".cancelFormQuestions").hide();
 
         var settings = {
           "url": "https://script.google.com/macros/s/AKfycbwF-Vfm9aeG4aNrGL2q1Yh9ZTumAtQA17tf2NnV_XjnovJiTSl1W5lK4leMyHK2r6w/exec",
