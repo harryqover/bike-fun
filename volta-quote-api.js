1c9fdbc1d45a2dc2ef6a61da694b89964c8ad773
harryqover <harry@qover.com>
2023-08-03 10:11:58 +0200

    Update volta-quote-api.js

--- volta-quote-api.js
+++ volta-quote-api.js
@@ -380,8 +380,8 @@ function translator(key, variables){
 function openSaveQuoteModal(quoteId, pack){
   console.log(quoteId, pack);
   var modal = '<section style="display: flex;" class="modal" id="modalgetquote"><div class="div-block-320"></div>';
-  modal = modal + '<div style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); filter: blur(0px); transform-style: preserve-3d;" class="w-layout-blockcontainer address-modal w-container" id="getquotediv">';
-    modal = modal + '<h4 class="h4-modal">'+translator("sendQuoteModalInfoText",{"pack": pack})+'You have selected the '+pack+' plan</h4>';
+  modal = modal + '<div style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); filter: blur(0px); transform-style: preserve-3d;" class="w-layout-blockcontainer address-modal w-container">';
+    modal = modal + '<div  id="getquotediv"><h4 class="h4-modal">'+translator("sendQuoteModalInfoText",{"pack": pack})+'You have selected the '+pack+' plan</h4>';
     modal = modal + '<h2 class="h2-second">'+translations.sendQuoteModalTitle+'</h2>';
     modal = modal + '<div class="w-form"><form id="getquote" class="flex-v for-modal">';
       modal = modal + '<div class="flex-h"><div class="rightaligned">'+translations.sendQuoteModalInputVAT+'</div><input type="text" class="input nomarginbottom w-input" maxlength="256" name="field-3" placeholder="" id="getquote-vat" required=""></div>';
@@ -392,7 +392,7 @@ function openSaveQuoteModal(quoteId, pack){
       modal = modal + '<input type="text" id="quoteId" value="'+quoteId+'" style="display:none">'
       modal = modal + '<input type="text" id="pack" value="'+pack+'" style="display:none">'
       modal = modal + '<a type="submit" class="button-primary-2 grey center w-button" id="sendQuoteBtn">'+translations.sendQuoteModalSubmit+'</a></form>';
-    modal = modal + '</div>';
+    modal = modal + '</div></div>';
     modal = modal + '<div class="div-close" id="closemodalgetquote">';
      modal = modal + '<img src="https://assets.website-files.com/644911ac1572f72efba69772/644922a7462df727411a64b5_cross.svg" loading="lazy" alt="" class="close-icon"><div>'+translations.sendQuoteModalClose+'</div>';
     modal = modal + '</div>';
