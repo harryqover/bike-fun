d83b29048b9a62fe1c9cded8ddcac8e0d804564d
harryqover <harry@qover.com>
2023-09-14 14:25:41 +0200

    Update volta-quote-api.js

--- volta-quote-api.js
+++ volta-quote-api.js
@@ -50,7 +50,7 @@ function clickToGetPrice(){
   console.log(quoteInfo);
 	getPrice(quoteInfo.truckAmount, quoteInfo.truckPrice, quoteInfo.dangerousGoodsTrucks, quoteInfo.leasedTrucks, quoteInfo.deductible_MTPL, quoteInfo.deductible_Casco, quoteInfo.country, quoteInfo.usage, quoteInfo.samePrice);
 }
-https://script.google.com/macros/s/AKfycbxd7iLSKEWjn4Kjsh6SR4zVDZCz2HxnAXA1OHG_7pzBrE0VLi5ze-9DOV1Y7tpyr13d6Q/exec
+
 const googleSheetUrl = "https://script.google.com/macros/s/AKfycbxd7iLSKEWjn4Kjsh6SR4zVDZCz2HxnAXA1OHG_7pzBrE0VLi5ze-9DOV1Y7tpyr13d6Q/exec";
 $("[data-price]").text("...");
 function getPrice(truckAmount, truckPrice, dangerousGoodsTrucks, leasedTrucks, deductible_MTPL, deductible_Casco, country, usage, samePrice) {
