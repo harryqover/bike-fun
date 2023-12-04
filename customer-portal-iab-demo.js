86239323cae45197d1720444b8395ef89672a832
harryqover <harry@qover.com>
2023-12-04 14:34:57 +0100

    Update customer-portal-iab-demo.js

--- customer-portal-iab-demo.js
+++ customer-portal-iab-demo.js
@@ -1,3 +1,27 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+function getParameterByName(name) { 
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]"); 
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), 
+        results = regex.exec(location.search); 
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " ")); 
+} 
+
+
 console.warn("v20231127 0917");
 
 const variants = {
