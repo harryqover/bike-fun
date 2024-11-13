f83775fce1e119a5bad5b66c3224893d21784836
harryqover <harry@qover.com>
2024-11-13 10:29:09 +0100

    Update volvoLanding.js

--- volvoLanding.js
+++ volvoLanding.js
@@ -1,4 +1,4 @@
-console.warn("1311 0910")
+console.warn("1311 1029")
 const webhookEmailSignup = "https://script.google.com/macros/s/AKfycbwM6BWIdthN5E-ov5HTjKnIl9m54ns0Y5FJMj0Svv6lKM-JbfyLjeE-W5PoARkt8w4Few/exec";
 
 setTimeout(function() {
@@ -13,7 +13,16 @@ setTimeout(function() {
             console.log('Vehicles data loaded:', vehicles);
             
             // Populate year options based on the vehicles' years
-            const years = [...new Set(vehicles.map(vehicle => vehicle.yearFrom))].sort((a, b) => b - a);
+            //const years = [...new Set(vehicles.map(vehicle => vehicle.yearFrom))].sort((a, b) => b - a);
+            const years = [
+              "2025", "2024", "2023", "2022", "2021", "2020", "2019", "2018", "2017", "2016", 
+              "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", 
+              "2005", "2004", "2003", "2002", "2001", "2000", "1999", "1998", "1997", "1996", 
+              "1995", "1994", "1993", "1992", "1991", "1990", "1989", "1988", "1987", "1986", 
+              "1985", "1984", "1983", "1982", "1981", "1980", "1979", "1978", "1977", "1976", 
+              "1975", "1974", "1973", "1972", "1971", "1970"
+            ];
+
             const yearFilter = $('#year-filter');
             years.forEach(year => {
                 yearFilter.append(`<option value="${year}">${year}</option>`);
