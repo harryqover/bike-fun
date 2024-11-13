7f583545c1b7177c4b6fa8d400dda8d5e36a9cf4
harryqover <harry@qover.com>
2024-11-13 09:10:41 +0100

    Update volvoLanding.js

--- volvoLanding.js
+++ volvoLanding.js
@@ -1,4 +1,4 @@
-console.warn("1118")
+console.warn("1311 0910")
 const webhookEmailSignup = "https://script.google.com/macros/s/AKfycbwM6BWIdthN5E-ov5HTjKnIl9m54ns0Y5FJMj0Svv6lKM-JbfyLjeE-W5PoARkt8w4Few/exec";
 
 setTimeout(function() {
