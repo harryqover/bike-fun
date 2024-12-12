5a9d457f078329976ef253302ff99b7ff33210c8
harryqover <harry@qover.com>
2024-12-12 17:19:55 +0100

    Update giftcard.js

--- giftcard.js
+++ giftcard.js
@@ -1,12 +1,9 @@
-console.log("up")
+console.log("up2")
 function getGiftCard(email) {
     if (!email) {
         var email = $("#email").val();
     }
-
-    // Show the loading message
-    $(".modal-gift-card .loading-message").show();
-    $(".modal-gift-card .success-message").hide();
+    $(".modal-gift-card .success-message").text("Loading....")
 
     var settings = {
         "url": "https://script.google.com/macros/s/AKfycbzAbLdRcpW6ZQFYJFpU-SEM6OiO_oT0GSiNjCrNKrZQObtfnMJYR8REBA_2uQv89zYY/exec",
@@ -23,12 +20,7 @@ function getGiftCard(email) {
     $.ajax(settings)
         .done(function(response) {
             console.log(response);
-
-            // Hide the loading message
-            $(".modal-gift-card .loading-message").hide();
-
-            // Show the success message
-            $(".modal-gift-card .success-message").text("Check your mailbox to get the code!").show();
+            $(".modal-gift-card").text("Check your mailbox to get the code!");
 
             // Hide the modal after 10 seconds
             setTimeout(function() {
@@ -37,18 +29,8 @@ function getGiftCard(email) {
         })
         .fail(function(error) {
             console.error("Error:", error);
-
-            // Hide the loading message and show an error message
-            $(".modal-gift-card .loading-message").hide();
-            $(".modal-gift-card .success-message").text("An error occurred. Please try again.").show();
+            $(".modal-gift-card .success-message").text("An error occurred. Please try again.");
         });
 }
 
-// HTML Example for modal content
-const modalHTML = `
-<div class="modal-gift-card">
-    <div class="loading-message" style="display: none;">Loading...</div>
-    <div class="success-message" style="display: none;"></div>
-</div>`;
 
-document.body.insertAdjacentHTML('beforeend', modalHTML);
