24f8da508d2251f2d6f849f1c933622346428a71
harryqover <harry@qover.com>
2025-03-24 15:18:44 +0100

    Update microlino-flow.js

--- microlino-flow.js
+++ microlino-flow.js
@@ -1,4 +1,4 @@
-console.log("20250324 1322")
+console.log("20250324 1518")
 // Mapping API error field names to form input names
 function getRootDomain(hostname) {
   const parts = hostname.split('.');
