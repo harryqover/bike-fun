5bfe391ac76b14df99f3c3679fe108e89a820342
harryqover <harry@qover.com>
2025-04-03 13:27:16 +0200

    Update bmm-dealer-portal.js

--- bmm-dealer-portal.js
+++ bmm-dealer-portal.js
@@ -1242,7 +1242,10 @@ document.getElementById('insuranceForm').addEventListener('submit', async functi
       console.log(response);
       console.log(response.payload.id);
       if([201,200].includes(response.payload.status)){
-        $("#message").html('<p class="success">Quote created successfully!</p><pre>' + JSON.stringify(response.payload.id, null, 2) + "</pre>");
+        $("#message").html('<p class="success">Quote created successfully!</p>');
+        if(domain == "webflow.io"){
+          $("#message").html('<p class="success">Quote created successfully!</p><pre>' + JSON.stringify(response.payload.id, null, 2) + "</pre>");
+        }
       } else {
         $("#message").html('<p class="success">An error occurred while creating the quote!</p><pre>' + JSON.stringify(response, null, 2) + "</pre>");
       }
