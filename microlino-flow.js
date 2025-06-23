4eedbe59e6fd747a0e8d6cb8da979f7c93d54f55
harryqover <harry@qover.com>
2025-06-23 15:50:05 +0200

    Update microlino-flow.js

--- microlino-flow.js
+++ microlino-flow.js
@@ -1307,16 +1307,21 @@ $("#quoteForm").on("submit", function(e) {
       sfClassMod = formData.get("sfClassMod");
   }
   
-  // If start date is to be set, validate it.
-  // A standard regex for email validation
-  const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  if (!email || !emailPattern.test(email)) {
-    $("#message").html('<p class="error">Bitte geben Sie eine gültige E-Mail-Adresse ein.</p>');
+  if (!email) {
+    $("#message").html('<p class="error">Bitte geben Sie eine E-Mail-Adresse ein.</p>');
     $("#loadingOverlay").hide();
     return;
   }
 
+  // A standard regex for email validation
+  const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!emailPattern.test(email)) {
+    $("#message").html('<p class="error">Das Format der E-Mail-Adresse ist ungültig. (z.B. name@beispiel.de)</p>');
+    $("#loadingOverlay").hide();
+    return;
+  }
+  // If start date is to be set, validate it.
   if(setStart) {
     if (!startDate) {
       $("#message").html('<p class="error">Bitte wählen Sie ein Startdatum.</p>');
