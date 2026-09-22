889b3704d8f953ef39f90412f047c20148ea191d
harryqover <harry@qover.com>
2026-09-22 08:38:42 +0200

    Update microlino-flow.js

--- microlino-flow.js
+++ microlino-flow.js
@@ -1536,7 +1536,7 @@ $("#quoteForm").on("submit", function(e) {
   // Build the payload with only tpl and mod coverages
   const payload = {
     productConfigurationId: "microlino",
-    partnerId: "67a9f3824ce2c52c73463db6",
+    //partnerId: "67a9f3824ce2c52c73463db6",
     country: country,
     language: language,
     renewal: {
